2f5015241d8928bb33a5d7225d11a24fbf856564
NoahBradderEDU <nobr7357@colorado.edu>
2023-11-27 20:40:21 -0700

    Updated wa12

--- wa12/wa12.js
+++ wa12/wa12.js
@@ -1,19 +1,45 @@
-let jokeSection = document.querySelector("js-quote-text");
-let generateBtn = document.querySelector("js-new-quote");
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
 
-let API ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+const questionTxt = document.querySelector('#js-quote-text');
+let answerTxt = document.querySelector('#js-answer-text');
 
-function acquireJoke() {
-   
-    jokeSection.classList.remove("content-fade");
-    
-    fetch(api)
-      .then((info) => info.json())
-      .then((item) => {
-        jokeSection.textContent = `${item.joke}`;
+let answer = '';
+
+// this is the endpoint for the API that we want to get a reponse from
+const endpoint = 'https://the-trivia-api.com/v2/questions';
+
+async function getQuote() {
+   // try -> tries something; if it returns an error, it puts us into the catch block
+    try {
+        const response = await fetch(endpoint);
+        // if !response.ok is "if the response ISN'T okay (status code 200)"
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
         
-        jokeSection.classList.add("content-fade");
-      });
-  }
+        // JSON is a dictionary, which is like a list; we call specific pieces of information out based on the 'key' associated with that value
+        displayQuote(json['question']);
+        answer = json['answer'];
+        answerTxt.textContent = '';
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
 
-  generateBtn.addEventListener("click", acquireJoke);
+// this function shows the question
+function displayQuote(question) {
+    questionTxt.textContent = question;
+}
+
+// this function shows the answer
+function displayAnswer() {
+    answerTxt.textContent = answer;
+}
+
+// we run getQuote once when the script first loads to populate a question
+// when the page is loading
+getQuote();
