b750c50cf65bbf97c37a1c834ed7dc15c73c3bee
NoahBradderEDU <nobr7357@colorado.edu>
2023-12-03 16:52:23 -0700

    Updated wa13

---
+++ wa13/wa13.js
@@ -0,0 +1,93 @@
+//Problem 1
+let company1 ={
+    "employees":[{
+    "firstName": "Sam",
+    "department": "Tech",
+    "designation": "Manager",
+    "salary": 40000,
+    "raiseElgible": "True"
+},
+{
+    "firstName": "Mary",
+    "department": "Finance",
+    "designation": "Trainee",
+    "salary": 18500,
+    "raiseElgible": "True"
+},
+{
+    "firstName": "Bill",
+    "department": "HR",
+    "designation": "Executive",
+    "salary": 21200,
+    "raiseElgible": "True"
+}]}
+
+//Problem 2
+
+let company2 ={
+    "companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees":[
+        {
+            "firstName": "Sam",
+            "department": "Tech",
+            "designation": "Manager",
+            "salary": 40000,
+            "raiseElgible": "True",
+        },
+        {
+            "firstName": "Mary",
+            "department": "Finance",
+            "designation": "Trainee",
+            "salary": 18500,
+            "raiseElgible": "True",
+        },
+        {
+            "firstName": "Bill",
+            "department": "HR",
+            "designation": "Executive",
+            "salary": 21200,
+            "raiseElgible": "True",
+        }
+    ]
+}
+
+//Problem 3
+
+let company3 = {
+    "companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employees":[
+        {
+            "firstName": "Sam",
+            "department": "Tech",
+            "designation": "Manager",
+            "salary": 40000,
+            "raiseElgible": "True",
+        },
+        {
+            "firstName": "Mary",
+            "department": "Finance",
+            "designation": "Trainee",
+            "salary": 18500,
+            "raiseElgible": "True",
+        },
+        {
+            "firstName": "Bill",
+            "department": "HR",
+            "designation": "Executive",
+            "salary": 21200,
+            "raiseElgible": "True",
+        },
+        {
+            "firstName": "Anna",
+            "department": "Tech",
+            "designation": "Executive",
+            "salary": 25600,
+            "raiseElgible": "False",   
+        }
+    ]
+}
+
+//Problem 4
+
