343e45e810890b080543cbddf64fa4e95f2a2514
NoahBradderEDU <nobr7357@colorado.edu>
2023-12-03 16:57:16 -0700

    Updated wa13

--- wa13/wa13.js
+++ wa13/wa13.js
@@ -1,93 +1,38 @@
-//Problem 1
-let company1 ={
-    "employees":[{
-    "firstName": "Sam",
-    "department": "Tech",
-    "designation": "Manager",
-    "salary": 40000,
-    "raiseElgible": "True"
-},
-{
-    "firstName": "Mary",
-    "department": "Finance",
-    "designation": "Trainee",
-    "salary": 18500,
-    "raiseElgible": "True"
-},
-{
-    "firstName": "Bill",
-    "department": "HR",
-    "designation": "Executive",
-    "salary": 21200,
-    "raiseElgible": "True"
-}]}
+// Problem 1: Create JSON for each employee
+const employees = [
+    { firstName: "Sam", department: "Tech", designation: "Manager", salary: 40000, raiseEligible: true },
+    { firstName: "Mary", department: "Finance", designation: "Trainee", salary: 18500, raiseEligible: true },
+    { firstName: "Bill", department: "HR", designation: "Executive", salary: 21200, raiseEligible: false }
+];
 
-//Problem 2
+// Problem 2: Create JSON for the company
+const company = {
+    companyName: "Tech Stars",
+    website: "www.techstars.site",
+    employees: employees
+};
 
-let company2 ={
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees":[
-        {
-            "firstName": "Sam",
-            "department": "Tech",
-            "designation": "Manager",
-            "salary": 40000,
-            "raiseElgible": "True",
-        },
-        {
-            "firstName": "Mary",
-            "department": "Finance",
-            "designation": "Trainee",
-            "salary": 18500,
-            "raiseElgible": "True",
-        },
-        {
-            "firstName": "Bill",
-            "department": "HR",
-            "designation": "Executive",
-            "salary": 21200,
-            "raiseElgible": "True",
-        }
-    ]
+// Problem 3: A new employee has joined the company
+const newEmployee = { firstName: "Anna", department: "Tech", designation: "Executive", salary: 25600, raiseEligible: false };
+company.employees.push(newEmployee);
+
+// Display the updated company JSON
+console.log("Updated Company JSON:", company);
+
+// Problem 4: Calculate the total salary for all company employees
+const totalSalary = company.employees.reduce((acc, employee) => acc + employee.salary, 0);
+console.log("Total Salary for all employees:", totalSalary);
+
+// Problem 5: Raise time - Update salary for raise-eligible employees and set eligibility to false
+function giveRaiseAndUpdateEligibility(employee) {
+    if (employee.raiseEligible) {
+        employee.salary *= 1.1; // Increase salary by 10%
+        employee.raiseEligible = false; // Set eligibility to false
+    }
 }
 
-//Problem 3
+// Apply raise to each employee in the company
+company.employees.forEach(giveRaiseAndUpdateEligibility);
 
-let company3 = {
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees":[
-        {
-            "firstName": "Sam",
-            "department": "Tech",
-            "designation": "Manager",
-            "salary": 40000,
-            "raiseElgible": "True",
-        },
-        {
-            "firstName": "Mary",
-            "department": "Finance",
-            "designation": "Trainee",
-            "salary": 18500,
-            "raiseElgible": "True",
-        },
-        {
-            "firstName": "Bill",
-            "department": "HR",
-            "designation": "Executive",
-            "salary": 21200,
-            "raiseElgible": "True",
-        },
-        {
-            "firstName": "Anna",
-            "department": "Tech",
-            "designation": "Executive",
-            "salary": 25600,
-            "raiseElgible": "False",   
-        }
-    ]
-}
-
-//Problem 4
-
+// Display the updated company JSON after the raise
+console.log("Company JSON after the raise:", company);
